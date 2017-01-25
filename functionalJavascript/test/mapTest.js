e03075ebeac5b1af6cee630faafd54c90cb4ba98
noobg1 <jeevu.art@gmail.com>
2017-01-25 09:28:01 +0530

    wrote test for map.js

--- functionalJavascript/test/mapTest.js
+++ functionalJavascript/test/mapTest.js
@@ -1,31 +1,44 @@
 var chai = require('chai');
 var expect = require('chai').expect;
-var testMap = require('../mapTest');
+var testMap = require('../map');
 
-describe('Check on map function usage taking array as output and giving array with each elemenst multiplied by 2', function () {
-  it('should return empty array', function () {
-    expect(testMap([])).to.eqls([]);
-  });
-  it('should be twice the array value', function () {
+describe('Checking on map function usage taking array as input and giving array as output with each element multiplied by 2 (valid inputs)', function () {
+
+  it('should return array with each value multiplied by 2', function () {
     expect(testMap([2, 3, 4, 5])).to.eqls([4, 6, 8, 10]);
   });
-  it('should be twice the array value with elements being string numbers', function () {
+  it('should return array with each value multiplied by 2 with string number as input', function () {
     expect(testMap(['2', '3', '4', '5'])).to.eqls([4, 6, 8, 10]);
   });
-  it('should be twice the array value with elements being string numbers', function () {
+  it('should be twice the array value with elements being string numbers with some input being string number', function () {
     expect(testMap(['2', '3', '4', 5])).to.eqls([4, 6, 8, 10]);
   });
-  it('should throw Invalid input', function () {
+  it('should return empty array on given empty array input', function () {
+    expect(testMap([])).to.eqls([]);
+  });
+
+});
+
+describe('Checking on map function usage taking array as input and giving array as output with each element multiplied by 2 (invalid inputs)', function () {
+
+  it('should throw error message on given one of the element being not a number', function () {
     expect(testMap(['2', '3', '4', 'fgddfg'])).to.eqls('Invalid input type');
   });
-  it('should throw Invalid input for non array', function () {
-    expect(testMap({})).to.eqls(undefined);
+  it('should throw error message for non array input (object)', function () {
+    expect(testMap({})).to.eqls('Invalid input type');
   });
-  it('should throw Invalid input for non array', function () {
-    expect(testMap({ id: '1' })).to.eqls(undefined);
+  it('should throw error message for non array input (object)', function () {
+    expect(testMap({ id: '1' })).to.eqls('Invalid input type');
   });
-  it('should throw Invalid input for non array', function () {
-    expect(testMap('random string')).to.eqls(undefined);
+  it('should throw error message for non array input (string)', function () {
+    expect(testMap('random string')).to.eqls('Invalid input type');
   });
+  it('should throw error message for non input/ no arguments', function () {
+    expect(testMap()).to.eqls('Invalid input type');
+  });
+  it('should throw error message for null input', function () {
+    expect(testMap(null)).to.eqls('Invalid input type');
+  });
+
 });
 
