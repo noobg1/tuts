35b9af5bb82a67aed59b91a3bf59af2ae20bde08
noobg1 <jeevu.art@gmail.com>
2017-01-25 09:28:32 +0530

    wrote test for map.js

--- functionalJavascript/map.js
+++ functionalJavascript/map.js
@@ -1,21 +1,22 @@
 function doubleAll(numbers) {
     var isValid = true, result, errorMessage = 'Invalid input type';
-    
-    function toMultiply(x)
-    {   if(isNaN(x)) isValid = false;
+
+    function toMultiply(x) {
+        if (isNaN(x)) isValid = false;
         return x * 2;
     }
-    
-    if(numbers instanceof Array) {
-    result = numbers.map(toMultiply);
-    }
-    else isvalid = false;
 
-    if(isValid)
+    if (numbers instanceof Array) {
+        result = numbers.map(toMultiply);
+    }
+    else isValid = false;
+
+    if (isValid) {
         return result;
-    else 
+    }
+    else
         return errorMessage;
-      
+
 }
 
 module.exports = doubleAll;
