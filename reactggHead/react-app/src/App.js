37db33ad66a398a05e5b62e14d206d5b2fef3725
noobg1 <jeevu.art@gmail.com>
2017-02-21 18:13:40 +0530

    state/ setState

--- reactggHead/react-app/src/App.js
+++ reactggHead/react-app/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      cat: 'this is state cat'
+    }
+  }
+
+  update( e ) {
+    this.setState({cat: e.target.value})
+  }
   render () {
-    return <h1>{this.props.txt}</h1>
+    return (
+      <div>
+        <input onChange={this.update.bind(this)}/>
+       <h1>{this.state.cat}</h1>
+      </div>)
   }
 }
 
