92f5e675b3a3d28cc6c6f0a6e2ad54cf285d68f3
noobg1 <jeevu.art@gmail.com>
2017-02-21 18:08:28 +0530

    default props/ proptypes

--- reactggHead/react-app/src/App.js
+++ reactggHead/react-app/src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 class App extends React.Component {
   render () {
-    return <h1>Hello...git </h1>
+    return <h1>{this.props.txt}</h1>
   }
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string.isRequired
+}
+
+App.defaultProps = {
+  txt: 'default text'
 }
 
 export default App
